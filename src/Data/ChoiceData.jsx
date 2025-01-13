a6fc496ba51cb2d1a432dcdd202077718f95ebab
Praise <oyeniyipraise@gmail.com>
2025-01-13 02:19:46 +0100

    ...

--- src/Data/ChoiceData.jsx
+++ src/Data/ChoiceData.jsx
@@ -10,28 +10,28 @@ export const Choice = [
         header:'Secure And Reliable',
         para:'SurePlug ensures secure transactions with advanced encryption and reliable service, giving you peace of mind with every payment.',
         image:Secure,
-        bgColor:'bg-[#6840ED]',
+        bgColor:'hover:bg-[#6840ED]',
         textColor:'text-[#6840ED]'
     },
     {
         header:'Fast And Easy Deposit',
         para:'Make quick and effortless deposits with SurePlug, allowing you to load funds instantly and get back to paying.',
         image:Fast,
-        bgColor:'bg-[#63B1FC]',
+        bgColor:'hover:bg-[#63B1FC]',
         textColor:'text-[#63B1FC]'
     },
     {
         header:'Saves Time',
         para:'Save time with SurePlug’s lightning-fast service process, allowing you to complete transactions in seconds, effortlessly.',
         image:Time,
-        bgColor:'bg-[#11BE36]',
+        bgColor:'hover:bg-[#11BE36]',
         textColor:'text-[#11BE36]'
     },
     {
         header:'Seamless Interface',
         para:'Enjoy a user-friendly interface designed for seamless navigation, making bill payments simple and intuitive for everyone.',
         image:Seamless,
-        bgColor:'bg-[#EC35A9]',
+        bgColor:'hover:bg-[#EC35A9]',
         textColor:'text-[#EC35A9]'
     },
 ]
